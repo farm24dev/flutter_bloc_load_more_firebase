1d1c79d6528b34f1dc1ea912b13ca2d824d090c7
WutthinanK <developer1@npappdesign.com>
2024-04-16 16:05:39 +0700

    init env

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_cubit_loadmore/app.dart';
 import 'package:flutter_cubit_loadmore/firebase_options.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
